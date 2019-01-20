f7f03c43e11cdc79f8a6235709822452face0274
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-01-20 23:22:43 +0530

    Create set2.10.c

---
+++ set2.10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n,sum;
+    scanf("%d",&n);
+    for(i=1;i<=5;i++)
+    {
+        sum=n*i;
+        printf("%d",sum);
+    }
+    getch();
+
+}
