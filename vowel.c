b944d38783c401494c5ded926391726e8c5435af
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-01-20 21:26:15 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char alpha;
+    scanf("%c",&alpha);
+    if(alpha=='a'||alpha=='e'||alpha=='i'||alpha=='o'||alpha=='u')
+{
+    printf("vowel");
+    
+}
+else
+{
+    printf("constant");
+}
+    return 0;
+}
