8798a43afd8316a7f6c108726e86729b2300fde2
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-01-20 19:31:46 +0530

    Create First.c

---
+++ First.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter num");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("positive");
+    }
+    else if(a<0)
+    {
+        printf("negative");
+    }
+    else
+    {
+        printf("zero");
+    }
+    
+
+    return 0;
+}
