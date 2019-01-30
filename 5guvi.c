6b49355c13e8198e53f24c39505121bdfb6178b9
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-01-30 07:46:57 +0530

    Create 5guvi.c

---
+++ 5guvi.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int main()
+{
+    
+    char s[1000000];
+    int len=0,i;
+    
+    scanf("%[^\n]",s);
+
+ 
+ for(i=0;s[i]!='\0';i++)
+ {
+     if(s[i]==' ')
+     {
+         len++;
+     }
+ }
+  printf("%d",len);
+    return 0;
+}
