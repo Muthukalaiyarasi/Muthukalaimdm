1e7e51f63eff625ea235935035c135b20f78e276
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-10 22:31:29 +0530

    Create set8.4.c

---
+++ set8.4.c
@@ -0,0 +1,16 @@
+
+
+#include<stdio.h>
+	#include <string.h>
+	
+
+	int main() {
+	  int u;
+	  float n;
+	  printf("enter the number");
+	  scanf("%f",n);
+	  u=(int)(n+0.5);
+	  printf("%d",u);
+		return 0;
+	}
+	
