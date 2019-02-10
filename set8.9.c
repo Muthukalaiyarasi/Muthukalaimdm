f87869615a8fb5e73b3c4e2b53c79ba931b7ebf0
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-10 22:24:23 +0530

    Create set8.9.c

---
+++ set8.9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+	int main(void)
+	{
+	int a,b,pro,n;
+	printf("enter the two number");
+	scanf("%d %d",&a,&b);
+	pro=a*b;
+	if(n=pro*pro)
+	{
+	    printf("yes");
+	}
+	else
+	{
+	  printf("no");
+	}
+	return 0;
+	}
+
+	
+
