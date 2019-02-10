bd549f47a1e93ec86ea1d66a38306515d37ea948
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-10 21:34:40 +0530

    Create set5.9.c

---
+++ set5.9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+	
+
+	int main()
+	{
+	  int a,b,sum;
+	  printf("enter the numbers");
+	  scanf("%d",&a);
+	  scanf("%d",&b);
+	  sum=a+b;
+	  
+	  printf("%d",sum);
+	
+
+	return 0;
+	}
