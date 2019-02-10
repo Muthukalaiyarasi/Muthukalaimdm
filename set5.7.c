a0dc8e0d6e339160bc6645891b56a12b01ae7b34
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-10 21:31:02 +0530

    Create set5.7.c

---
+++ set5.7.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+	
+
+	int main()
+	{
+	  int a[20],n,i,max,min;
+	  printf("enter the number");
+	  scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	  scanf("%d",&a[i]);
+	}
+	max=a[0];
+	min=a[0];
+	  for(i=0;i<n;i++)
+	  {
+	if(a[i]>max)
+	{
+	  max=a[i];
+	  }
+	  if(a[i]<min)
+	  {
+	    min=a[i];
+	  }
+	  }
+	  printf("%d",max);
+	  printf("%d",min);
+	  return 0;
+	  }
+	  
