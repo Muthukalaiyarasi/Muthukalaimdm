4688af3325e556aaf1625a51d1c96abfc3353fd0
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-10 22:15:24 +0530

    Create set7.4.c

---
+++ set7.4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+	int main()
+	{
+	  
+	  int a,b,sum;
+	printf("enter the numbers");
+	
+
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	if(sum%2==0)
+	{
+	printf("even");
+	}
+	else
+	{
+	  printf("odd");
+	}
+	return 0;
+	}
