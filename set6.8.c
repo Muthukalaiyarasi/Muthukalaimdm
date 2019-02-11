99777944fe742ecfa92a784a211851bf11142f37
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-11 07:57:47 +0530

    Create set6.8.c

---
+++ set6.8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+	
+	int main()
+	{
+	    int n,k,c=0,a[50],i;
+	    scanf("%d%d",&n,&k);
+	    for(i=0;i<n;i++)
+	    {
+	        scanf("%d",&a[i]);
+	    }
+	    for(i=0;i<n;i++)
+	    {
+	        if(a[i]==k)
+	        {
+	            c++;
+	        }
+	    }
+	    if(c>0)
+	    {
+	    printf("yes");
+	    }
+	    else
+	    {
+	        printf("no");
+	    }
+	    return 0;
+	}
+
+
+
