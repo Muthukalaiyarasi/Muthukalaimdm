d44d08b988e371aa7c211f311f892a18fef4c770
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-11 08:00:49 +0530

    Create set6.10.c

---
+++ set6.10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+	
+	int main()
+	{
+	    int num,c=0,i;
+	    scanf("%d",&num);
+	    for(i=1;i<=num;i++)
+	    {
+	        c=c+i;
+	    }
+	    printf("%d",c);
+	    return 0;
+	}
