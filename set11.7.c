fe754f3ed5400392a2f496282f70cc0d0ed0d9ce
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-12 21:57:31 +0530

    Create set11.7.c

---
+++ set11.7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+	
+	int main()
+	{
+	    int a,b,c,ans;
+      printf("enter the number");
+      scanf("%d %d %d",&a,&b,&c);
+      ans=(a*b)/c;
+      printf("%d",ans);
+
+	    return 0;
+}
