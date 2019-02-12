cade2857a6a049346cd751f691f1f70180d0379e
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-12 21:55:14 +0530

    Create set11.5.c

---
+++ set11.5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+	
+	int main()
+	{
+	    int num,k;
+      
+      printf("enter the number");
+      scanf("%d %d",&num,&k);
+      
+      printf("%d%d",num,k);
+
+	    return 0;
+}
