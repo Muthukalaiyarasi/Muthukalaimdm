1f07da17a7df2c22ecdd5203e7a563292e1ea6dc
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-19 15:56:07 +0530

    Create playset4.2.c

---
+++ playset4.2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+  int a[10000],n,i,f=0,k;
+  scanf("%d %d",&n,&k);
+for(i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+}
+for(i=0;a[i]!='\0';i++)
+{
+  if(a[i]==k)
+  {
+    f=1;
+    break;
+  }
+}
+if(f==1)
+{
+  printf("yes");
+}
+else
+{
+ printf("no") ;
+}
+  return 0;
+}
