ec3193d071c9d42d8d754053e3adbc13fc476f8f
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-24 21:27:13 +0530

    Create play47.c

---
+++ play47.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,s=0;
+	  scanf("%d %d %d",&a,&b,&c);
+    s=a+b+c;
+    if(s==180)
+    {
+        printf("yes..it forms traingle");
+    }
+    else
+    {
+        printf("they cant be the interior angles of the triangle");
+    }
+	return 0;
+}
