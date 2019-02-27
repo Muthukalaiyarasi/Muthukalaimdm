0c2a0d61cddc5d0a03c4524d99e5590c5b6df85c
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-02-27 12:08:37 +0530

    Create play55.c

---
+++ play55.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    char a[10],b[10];
+    int i,j,ct=0;
+    scanf("%s %s",a,b);
+    for(i=0,j=0;a[i]!='\0',b[i]!='\0';i++,j++)
+    {
+        if(a[i]=b[i])
+        {
+            c=1;
+        }
+        else
+        {
+            printf("no");
+            c=0;
+            break;
+        }
+    }
+    if(c==1)
+    {
+    printf("yes both strings are the same");
+    }
+   return 0;
+}
