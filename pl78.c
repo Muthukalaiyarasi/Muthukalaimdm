037d4e3424afb9023d25f63848aeea83151a4a84
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-03-12 09:47:19 +0530

    Create pl78.c

---
+++ pl78.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int N,M,ar[100000],b[10000],c[100000],i,j,k=0,f;
+   scanf("%d %d",&N,&M);
+ for(i=0;i<N;i++)
+   {
+       scanf("%d",&ar[i]);
+       c[k]=ar[i];
+       k++;
+   }
+      for(i=0;i<M;i++)
+   {
+       scanf("%d",&b[i]);
+       c[k]=b[i];
+       k++;
+   }
+       for(i=0;i<k;i++)
+   { 
+   for(j=i;j<k;j++)
+   {
+   if(c[i]>c[j] && c[i]==c[j])
+   {
+       f=c[i];
+       c[i]=c[j];
+       c[j]=f;    
+   }
+   }
+   }
+       for(i=0;i<k;i++)
+   {
+       printf("%d ",c[i]);
+   }
+  
+    return 0;
+}
