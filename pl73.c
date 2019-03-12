6358e1deb1699f9f553199e5c73462ab37d3cb7e
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-03-12 09:57:14 +0530

    Create pl73.c

---
+++ pl73.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+int NUM,Arr[500],Brr[500],i,j,c=0;
+scanf("%d",&NUM);
+for(i=0;i<NUM;i++)
+{
+    scanf("%d",&Arr[i]);
+}
+
+for(i=0;i<NUM;i++)
+{
+    scanf("%d",&Brr[i]);
+}
+ for(i=0;i<NUM;i++)
+ {
+     for(j=0;j<NUM;j++)
+ { 
+    if(Arr[i]==Brr[j])
+     {
+     break;   
+     }   
+ }
+ if(c!=1)
+ { printf("%d ",Arr[i]); }
+ }
+ return 0; 
+}
