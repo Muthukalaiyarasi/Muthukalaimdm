d2c878ae5355158d3c8da24de52381668fb54eec
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-03-12 07:32:40 +0530

    Create pl9.2.c

---
+++ pl9.2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+	int n,t=0;
+	scanf("%d",&n);
+	int a[n];
+	int i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		t=t&a[i];
+	}
+	printf("%d",t);
+	
+
+	return 0;
+}
