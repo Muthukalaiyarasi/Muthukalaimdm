6e966d062d389b1f27d2c29212c204a4ea0fbb15
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-03-18 08:02:36 +0530

    Create pl13.10.c

---
+++ pl13.10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int N,i,j,s=0,k;
+scanf("%d",&N);
+int a[N];
+for(i=0;i<N;i++)
+{
+    scanf("%d",&a[i]);
+}
+  
+  
+ 
+  
+for(i=0;i<N;i++)
+   {
+       
+       
+  
+    s=s+a[i];
+    
+    if(sum%2==0)
+    printf("%d ",s);
+    else
+      printf("%d ",s);
+       
+}
+  
+return 0;
+}
