bf642f2b7cf486d9782974568e207c8d0ec0ad08
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-03-18 07:23:05 +0530

    Create pl11.1.c

---
+++ pl11.1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int n,a[100],s=0,i;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=1;i<n;i++)
+   {
+   s=s+a[i];
+   
+   }
+   printf("%d",s);
+}
