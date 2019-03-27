8a085160d778df48a7a2445fc1831c26d9b76998
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-03-27 18:54:32 +0530

    Create pl161.c

---
+++ pl161.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() 
+{
+
+char s[100];
+	int n,i,j,flag=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+       gets(s);
+
+	for(i=0;i<n;i++)
+	{
+		for(j=0;s[j]!='\0';j++)
+		{
+			if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U')
+			{
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		break;
+	
+	}
+	if(flag==0)
+		printf("no");
+
+	else
+		printf("yes");
+
+}
