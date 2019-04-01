89a41aaafc8df97db3c44591ad1b10d6c05f42e4
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-04-01 15:43:32 +0530

    Rename huntset1.2.c to hunterset1.2.c

---
+++ hunterset1.2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+   int i,j,b[100],k,m;
+   scanf("%d",&k);
+   for(i=0;i<k;i++)
+   {
+       scanf("%d",&b[i]);
+   }
+   
+   
+  
+   
+
+    for(i=0;i<k;i++)
+   {
+       
+   for(j=i+1;j<k;j++)
+   {
+     
+           
+             
+         if(b[i]<b[j])
+         {
+             
+         m=b[i];
+         b[i]=b[j];
+         b[j]=m;
+         
+           
+           
+           
+    
+   }}}
+   for(i=0;i<k;i++)
+   {
+    
+    
+    printf("%d",b[i]);
+   }
+   
+   
+    return 0;
+}
