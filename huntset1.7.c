a7f25f1098518960b5538f5f9abd3e94f8071376
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-04-03 15:27:04 +0530

    Create huntset1.7.c

---
+++ huntset1.7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+  int a[1000],i,n;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+
+  }
+  
+  for(i=0;i<n;i++)
+  {
+    if(a[i]%2!=0)
+    {
+      printf("%d ",a[i]);
+    }
+  
+  
+    else
+    {
+      printf("%d ",a[i]);
+    }
+  }
+  return 0;
+}
