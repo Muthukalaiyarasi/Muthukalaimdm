83fa035715d9771d48916dab5386f6237c27f248
Muthukalaiyarasi <46838655+Muthukalaiyarasi@users.noreply.github.com>
2019-04-03 15:43:25 +0530

    Create huntaet1.8.c

---
+++ huntaet1.8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+  int a[1000],i,n,j,k;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+
+  }
+  
+  for(i=0;i<n;i++)
+  {
+    for(j=i+1;j<n;j++)
+    {
+   for(k=j+1;k<n;k++)
+   {
+   if((a[i]+a[j]==a[k]) && i<j<k)
+   {
+     printf("%d %d %d\n",a[i],a[j],a[k]);
+   }
+   }
+    }
+  }
+  
+      
+  return 0;
+}
